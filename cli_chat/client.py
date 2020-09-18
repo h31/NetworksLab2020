045393a5446b3a27b3ee528ab133e87eddb3e545
Nikita Ineshin <ineshin.no@gmail.com>
2020-09-18 22:37:39 +0300

    Add files via upload
    Created settings files

--- cli_chat/client.py
+++ cli_chat/client.py
@@ -2,18 +2,34 @@ import socket
 import errno
 import sys
 import threading
-import time
+import json
 
-HEADER_LENGTH = 10
+#Load config file
+try:
+	with open('client_config.json') as config:
+		settings = json.load(config)
+except:
+	print('Config file does not exists. Default config file will be created.')
 
-IP = '127.0.0.1'
-PORT = 9999
-CODE = 'utf-8'
+	with open('client_config.json', 'w') as default_config:
+		settings =  {
+			"ip": "127.0.0.1",
+    		"port": 9999,
+    		"code": "utf-8"
+		}
+		json.dump(settings, default_config)
+
+
+IP = settings["ip"]
+PORT = settings["port"]
+CODE = settings["code"]
+
+HEADER_LENGTH = 8
+TIME_LENGTH = 8
 
 input_username = input("Enter username: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
 username = input_username.encode(CODE)
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode(CODE)
@@ -22,7 +38,6 @@ client_socket.send(username_header + username)
 
 
 def write():
-
 	while True:
 		message = input()
 
@@ -49,20 +64,15 @@ def receive():
 
 			message = client_socket.recv(message_length).decode(CODE)
 
-			message_time = time.strftime("%H:%M:%S", time.localtime())
+			time = client_socket.recv(TIME_LENGTH).decode(CODE)
 
-			print(f'<{message_time}> [{username}]: {message}')
+			print(f'<{time}> [{username}]: {message}')
 
-
-		except IOError as e:
-			if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-				print('IO Error', str(e))
-				sys.exit()
-			continue
 
 		except Exception as e:
 			print('Error', str(e))
 			sys.exit()
+
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()		
--- cli_chat/server.py
+++ cli_chat/server.py
@@ -1,10 +1,30 @@
 import socket
 import select
+import json
+import time
 
-HEADER_LENGTH = 10
-IP = '127.0.0.1'
-PORT = 9999
-CODE = 'utf-8'
+#Load config file
+try:
+	with open('server_config.json') as config:
+		settings = json.load(config)
+except:
+	print('Config file does not exists. Default config file will be created.')
+
+	with open('server_config.json', 'w') as default_config:
+		settings =  {
+			"ip": "127.0.0.1",
+    		"port": 9999,
+    		"code": "utf-8"
+		}
+
+		json.dump(settings, default_config)
+		
+
+IP = settings["ip"]
+PORT = settings["port"]
+CODE = settings["code"]
+
+HEADER_LENGTH = 8
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -30,6 +50,7 @@ def receive_message(client_socket):
 	except:
 		return False
 
+
 while True:
 	read_sockets, n_, exception_sockets = select.select(sockets_list, [], sockets_list)
 
@@ -44,7 +65,8 @@ while True:
 			sockets_list.append(client_socket)
 			clients[client_socket] = user
 
-			print(f"New connetion from {client_address[0]}:{client_address[1]} Username: {user['data'].decode(CODE)}")
+			print(f"New connetion from {client_address[0]}:{client_address[1]} "
+			 f"Username: {user['data'].decode(CODE)}")
 
 		else:
 			message = receive_message(notified_socket)
@@ -60,9 +82,11 @@ while True:
 
 			print(f"Received message from {user['data'].decode(CODE)}: {message['data'].decode(CODE)}")
 
+			message_time = time.strftime("%H:%M:%S", time.localtime()).encode(CODE)
+
 			for client_socket in clients:
 				if client_socket != notified_socket:
-					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'] + message_time)
 
 	for notified_socket in exception_sockets:
 		sockets_list.remove(notified_socket)
