a081c65fc4fd036c9aef3fff5fd87a4d4aaa320a
Violetta Mamykina <violetta-1998m@mail.ru>
2020-09-19 22:22:58 +0300

    Client's communication

--- tcp_template/src/client.py
+++ tcp_template/src/client.py
@@ -1,15 +1,27 @@
 import socket
+import threading
 
 
-def connect():
-    port = 5001
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('localhost', port))
-    s.send("Hello cat".encode('utf-8'))
-    data = s.recv(1024).decode('utf-8')
-    print(data)
-    s.close()
+class ThreadReceive(threading.Thread):
+    def __init__(self, server_socket):
+        threading.Thread.__init__(self)
+        self.ssocket = server_socket
+
+    def run(self):
+        while True:
+            data = self.ssocket.recv(1024).decode('utf-8')
+            print(data)
+            if data == b'':
+                break
 
 
-if __name__ == '__main__':
-    connect()
+SERVER = "127.0.0.1"
+PORT = 5001
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
+while True:
+    thread_receive = ThreadReceive(client)
+    thread_receive.start()
+    out_data = input()
+    client.send(bytes(out_data, 'UTF-8'))
+
--- tcp_template/src/server.py
+++ tcp_template/src/server.py
@@ -1,20 +1,59 @@
 import socket
+import threading
+
+all_sock = {}
+data = ""
 
 
-def connect():
-    port = 5001
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('', port))
-    serversocket.listen(5)
-    (clientsocket, address) = serversocket.accept()
-    while True:
-        data = clientsocket.recv(1024).decode('utf-8')
-        if data == b'':
-            break
-        clientsocket.send("Получил".encode('utf-8'))
-        print(data)
-    clientsocket.close()
+def sending_to_all(msg):
+    global all_sock
+    # global data
+    for a in all_sock.keys():
+        all_sock[a].send(msg)
+
+
+class ThreadReceive(threading.Thread):
+    def __init__(self, client_address, client_socket):
+        threading.Thread.__init__(self)
+        self.csocket = client_socket
+
+    def run(self):
+        global all_sock
+        all_sock[client_address] = clientsock
+        global data
+        self.csocket.send("Hello from Letta".encode('UTF-8'))
+        while True:
+            data = self.csocket.recv(1024).decode('utf-8')
+            if data == b'':
+                break
+            print("from client", data)
+        # print("Client at ", client_address, "disconnected...")
+
+
+class ThreadSend(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            global data
+            if data != '':
+                sending_to_all(bytes(data, 'UTF-8'))
+                data = ""
 
 
 if __name__ == '__main__':
-    connect()
+    LOCALHOST = "127.0.0.1"
+    PORT = 5001
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((LOCALHOST, PORT))
+    print("Server started")
+    server.listen(5)
+    thread_send = ThreadSend()
+    thread_send.start()
+    while True:
+        clientsock, client_address = server.accept()
+        thread_receive = ThreadReceive(client_address, clientsock)
+        thread_receive.start()
+
