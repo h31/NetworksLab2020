cb1b2f02849bd64b84a4f580dd05129013ab9270
Violetta Mamykina <violetta-1998m@mail.ru>
2020-09-28 22:58:13 +0300

    Add length of message

--- tcp_template/src/client.py
+++ tcp_template/src/client.py
@@ -9,10 +9,17 @@ class ThreadReceive(threading.Thread):
 
     def run(self):
         while True:
-            data = self.ssocket.recv(1024).decode('UTF-8')
-            print(data)
-            if data == b'':
-                break
+            length_message = int(self.ssocket.recv(8).decode('UTF-8'))
+            chunks = []
+            bytes_recd = 0
+            while bytes_recd < length_message:
+                chunk = self.ssocket.recv(length_message)
+                if chunk == b'':
+                    raise RuntimeError("socket connection broken")
+                chunks.append(chunk)
+                bytes_recd = bytes_recd + len(chunk)
+            data = b''.join(chunks)
+            print(data.decode("UTF-8"))
 
 
 SERVER = "127.0.0.1"
@@ -21,11 +28,14 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 login = input("Enter your login: ")
 client.send(bytes(login, 'UTF-8'))
+thread_receive = ThreadReceive(client)
+thread_receive.start()
 while True:
-    thread_receive = ThreadReceive(client)
-    thread_receive.start()
     out_data = input()
-    length = len(out_data)
+    length = '{:08d}'.format(len(out_data))
     client.send(bytes(str(length), 'UTF-8'))
     client.send(bytes(out_data, 'UTF-8'))
-#client.close()
+    if out_data == 'leave chat':
+        break
+client.shutdown(socket.SHUT_WR)
+client.close()
--- tcp_template/src/server.py
+++ tcp_template/src/server.py
@@ -7,6 +7,7 @@ socket_from = socket
 client_login = ""
 names = {}
 length = 0
+length_length = 8
 
 
 def to_bytes(text):
@@ -25,37 +26,46 @@ def sending_to_all(msg, sock):
     return len(login_bytes+msg)
 
 
+def sending_to_all_without_name(msg, sock):
+    global all_sock
+    global socket_from
+    for a in all_sock.keys():
+        if all_sock[a] != sock:
+            all_sock[a].send(msg)
+
+
 class ThreadReceive(threading.Thread):
     def __init__(self, client_address, client_socket):
         threading.Thread.__init__(self)
         self.csocket = client_socket
+        self.cadrress = client_address
 
     def run(self):
         global all_sock
-        all_sock[client_address] = client_sock
+        all_sock[self.cadrress] = self.csocket
         global data
         global length
         global socket_from
         global client_login
         global names
-        client_login = self.csocket.recv(1024).decode('UTF-8')
+        client_login = self.csocket.recv(2048).decode('UTF-8')
         names[self.csocket] = client_login
         while True:
-            length = self.csocket.recv(1024).decode('UTF-8')
+            length = int(self.csocket.recv(length_length).decode('UTF-8'))
             print(length)
             socket_from = self.csocket
             chunks = []
             bytes_recd = 0
-            while bytes_recd < int(length):
-                chunk = self.csocket.recv(min(int(length) - bytes_recd, 2048))
+            while bytes_recd < length:
+                chunk = self.csocket.recv(length)
                 if chunk == b'':
                     raise RuntimeError("socket connection broken")
+                if chunk == "leave chat":
+                    break
                 chunks.append(chunk)
                 bytes_recd = bytes_recd + len(chunk)
             data = b''.join(chunks)
             print("message from client", data)
-
-    # print("Client at ", client_address, "disconnected...")
 
 
 class ThreadSend(threading.Thread):
@@ -67,15 +77,19 @@ class ThreadSend(threading.Thread):
         while True:
             total_sent = 0
             global data
+            global names
             global socket_from
             global client_login
             while total_sent < int(length):
                 if data != '':
+                    length_name_message = '{:08d}'.format((int(length)+len(names[socket_from])+3))
+                    sending_to_all_without_name(to_bytes(length_name_message), socket_from)
                     sent = sending_to_all(data[total_sent:], socket_from)
                     data = ""
                     if sent == 0:
                         raise RuntimeError("socket connection broken")
                     total_sent = total_sent + sent
+                    print("send length"+length_name_message)
 
 
 if __name__ == '__main__':
