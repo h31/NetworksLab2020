cd64fcc830d45259efa87c80ce1351a18da88f42
mourinhan8 <sbtc.walkingdead@gmail.com>
2020-10-08 15:44:09 +0300

    Correct format of text

--- tcp_lab/client.c
+++ tcp_lab/client.c
@@ -11,10 +11,13 @@
 
 char msg[MAXDATASIZE];
 
-void trimString (char str[32], int length){
-	for (int i = 0; i < length; i++) {
+void trimString(char str[32], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
 		// trim \n
-		if (str[i] == '\n') {
+		if (str[i] == '\n')
+		{
 			str[i] = '\0';
 			break;
 		}
@@ -27,13 +30,15 @@ void *recvmg(void *my_sock)
 	int len;
 	bzero(msg, MAXDATASIZE);
 	// Client thread always ready to receive message
-	while((len = recv(sock, msg, MAXDATASIZE, 0)) > 0) {
+	while ((len = recv(sock, msg, MAXDATASIZE, 0)) > 0)
+	{
 		msg[len] = '\0';
 		fputs(msg, stdout);
 	}
 }
 
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[])
+{
 	pthread_t recvt;
 	int sockfd;
 	char buf[MAXDATASIZE];
@@ -50,40 +55,44 @@ int main(int argc,char *argv[]){
 	trimString(clientName, strlen(clientName));
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (sockfd < 0) {
+
+	if (sockfd < 0)
+	{
 		printf("ERROR opening socket\n");
 		exit(1);
 	}
 
 	memset(&serverIP, '0', sizeof(serverIP));
 	serverIP.sin_port = htons(port);
-	serverIP.sin_family= AF_INET;
+	serverIP.sin_family = AF_INET;
 	serverIP.sin_addr.s_addr = inet_addr(ip);
 
-	if( (connect(sockfd, (struct sockaddr *)&serverIP,sizeof(serverIP))) == -1 ) {
+	if ((connect(sockfd, (struct sockaddr *)&serverIP, sizeof(serverIP))) == -1)
+	{
 		close(sockfd);
 		printf("ERROR connection to socket failed\n");
 		return 1;
 	}
-		
+
 	// Creating a client thread which is always waiting for a message
 	pthread_create(&recvt, NULL, (void *)recvmg, &sockfd);
 
 	struct tm *tm = localtime(&t);
-	
+
 	// Ready to read a message from console
-	while(fgets(msg, MAXDATASIZE, stdin) > 0) {
+	while (fgets(msg, MAXDATASIZE, stdin) > 0)
+	{
 		bzero(buf, MAXDATASIZE);
 		strftime(buf, MAXDATASIZE, "<%H:%M> ", tm);
 		strcat(strcat(strcat(buf, clientName), " : "), msg);
-		if(write(sockfd, buf, strlen(buf)) < 0) {
+		if (write(sockfd, buf, strlen(buf)) < 0)
+		{
 			printf("ERROR writing to socket\n");
 			return 1;
 		}
 		bzero(msg, MAXDATASIZE);
 	}
-	
+
 	// Thread is closed
 	pthread_join(recvt, NULL);
 	close(sockfd);
--- tcp_lab/server.c
+++ tcp_lab/server.c
@@ -7,17 +7,21 @@
 
 #define MAXDATASIZE 256
 
-//Global variables
+// Global variables
 pthread_mutex_t threadChat;
 int clients[20];
 int n = 0;
 
-void sendToClient(char *msg, int tmp){
+void sendToClient(char *msg, int tmp)
+{
 	int i;
 	pthread_mutex_lock(&threadChat);
-	for(i = 0; i < n; i++) {
-		if(clients[i] != tmp) {
-			if(send(clients[i], msg, strlen(msg), 0) < 0) {
+	for (i = 0; i < n; i++)
+	{
+		if (clients[i] != tmp)
+		{
+			if (send(clients[i], msg, strlen(msg), 0) < 0)
+			{
 				printf("ERROR sending to clients\n");
 				continue;
 			}
@@ -26,30 +30,33 @@ void sendToClient(char *msg, int tmp){
 	pthread_mutex_unlock(&threadChat);
 }
 
-void *recvmg(void *client_sock){
+void *recvmg(void *client_sock)
+{
 	int sock = *((int *)client_sock);
 	char msg[MAXDATASIZE];
 	bzero(msg, MAXDATASIZE);
 	int len;
-	while((len = recv(sock, msg, MAXDATASIZE, 0)) > 0) {
+	while ((len = recv(sock, msg, MAXDATASIZE, 0)) > 0)
+	{
 		msg[len] = '\0';
 		sendToClient(msg, sock);
 	}
-	
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	struct sockaddr_in serverIP;
 	pthread_t recvt;
 	char *ip = "127.0.0.1";
 	int sockfd = 0, clientSock = 0;
 	uint16_t portno;
-	
+
 	portno = atoi(argv[1]);
-	
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (sockfd < 0) {
+	if (sockfd < 0)
+	{
 		printf("ERROR opening socket");
 		exit(1);
 	}
@@ -59,28 +66,29 @@ int main(int argc, char *argv[]){
 	serverIP.sin_port = htons(portno);
 	serverIP.sin_addr.s_addr = inet_addr(ip);
 
-	/* bind the host address using bind() call.*/
-	if(bind( sockfd, (struct sockaddr *)&serverIP, sizeof(serverIP)) == -1 )
+	// Bind the host address using bind() call
+	if (bind(sockfd, (struct sockaddr *)&serverIP, sizeof(serverIP)) == -1)
 		printf("ERROR on binding\n");
 	else
 		printf("Server Started\n");
-	
-	if(listen(sockfd, 20) == -1 )
+
+	if (listen(sockfd, 20) == -1)
 		printf("ERROR listening\n");
-		
-	while(1){
+
+	while (1)
+	{
 		struct sockaddr_in cliAddr;
 		unsigned int clilen;
 		clilen = sizeof(cliAddr);
 		memset(&cliAddr, '0', sizeof(cliAddr));
-		if((clientSock = accept(sockfd, (struct sockaddr *) &cliAddr, &clilen)) < 0 )
+		if ((clientSock = accept(sockfd, (struct sockaddr *)&cliAddr, &clilen)) < 0)
 			printf("ERROR on accept\n");
 		pthread_mutex_lock(&threadChat);
 		clients[n] = clientSock;
 		n++;
-		// Creating a thread for each client 
+		// Creating a thread for each client
 		pthread_create(&recvt, NULL, (void *)recvmg, &clientSock);
 		pthread_mutex_unlock(&threadChat);
 	}
-	return 0; 
+	return 0;
 }
