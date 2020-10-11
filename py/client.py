da69ac3f88c07fa9a720d553fdf3ec01ce9da8ce
tdw <tdw31@tdw.localdomain>
2020-10-11 23:14:38 +0300

    limit length of message + add readline to handle arrow key

--- py/client.py
+++ py/client.py
@@ -1,62 +1,89 @@
-import socket
-import threading
-import json
-import sys
 import os
-HOST ='127.0.0.1'
-PORT =5002
-M_SIZE=1024
+import json
+import threading
+import socket
+import readline
+HOST = '127.0.0.1'
+PORT = 5003
+M_SIZE = 1024
 
 #send to server
+
+
 def send_to_sv(cli_socket):
     while True:
+
         inp = input()
-        if inp=='out_chat':
+        if inp == 'out_chat':
             out_chat(cli_socket)
         else:
-            normal_chat(cli_socket,inp)
-    
+            normal_chat(cli_socket, inp)
+
 #out of chat
+
+
 def out_chat(cli_socket):
-    msg={'type':'O','msg':''}
-    cli_socket.send(json.dumps(msg).encode('utf8', 'error input'))   
+    msg = {'type': 'O', 'msg': ''}
+    cli_socket.send(json.dumps(msg).encode('utf8', 'error input'))
     os._exit(0)
 
 #chat as normal
-def normal_chat(cli_socket,inp):
-    msg={'type':'N','msg':inp}  
-    cli_socket.send(json.dumps(msg).encode('utf8', 'error input')) 
+
+
+def normal_chat(cli_socket, inp):
+    msg = {'type': 'N', 'msg': inp}
+    msg = json.dumps(msg).encode('utf8', 'error input')
+    if len(msg) > 1024:
+        print('>>>MESSAGE TOO LONG. CAN NOT SEND')
+        return
+    cli_socket.send(msg)
 
 #receive message from server
+
+
 def rc_fr_sv(cli_socket):
     while True:
-        msg=cli_socket.recv(M_SIZE)
+        msg = cli_socket.recv(M_SIZE)
         #server down suddenly(keyboard interupt)
-        if (msg==b'-1') or (len(msg)==0):
-                print('server down')
-                cli_socket.close()            
-                os._exit(0)
+        if (msg == b'-1') or (len(msg) == 0):
+            print('server down')
+            cli_socket.close()
+            os._exit(0)
         print(msg.decode())
 
 #client
+
+
 def cl():
-    cli_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cli_socket.connect((HOST,PORT)) 
+    cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cli_socket.connect((HOST, PORT))
     #keyboard interrupt when insert name of user
-    try:
-        cli_name= input('Your Name(Shinkai Makoto):')
-    except KeyboardInterrupt:
-        out_chat(cli_socket)
-        os._exit()
-    msg={'type':'J','msg':cli_name}
-    cli_socket.send(json.dumps(msg).encode('utf8', 'error input'))   
+    while True:
+        try:
+            cli_name = input('Your Name(Shinkai Makoto):')
+        except KeyboardInterrupt:
+            out_chat(cli_socket)
+            os._exit(0)
+        if cli_name == '':
+            cli_name = 'anony'
+            break
+        elif len(cli_name) > 32:
+            print('>>NAME TOO LONG<<')
+        else:
+            break
+    msg = {'type': 'J', 'msg': cli_name}
+    cli_socket.send(json.dumps(msg).encode('utf8', 'error input'))
     send_th = threading.Thread(target=send_to_sv, args=[cli_socket])
     send_th.start()
-    rc_th=threading.Thread(target=rc_fr_sv, args=[cli_socket])
+    rc_th = threading.Thread(target=rc_fr_sv, args=[cli_socket])
     rc_th.start()
     try:
         while 1:
+
             continue
     except KeyboardInterrupt:
         out_chat(cli_socket)
+
+
 cl()
+
--- py/server.py
+++ py/server.py
@@ -4,60 +4,70 @@ import json
 from datetime import datetime
 import os
 
-HOST ='127.0.0.1'
-PORT =5002
-M_SIZE=1024
-client_list=[]
-list_name=[]
+HOST = '127.0.0.1'
+PORT = 5003
+M_SIZE = 1024
+client_list = []
+list_name = []
 #accept client_socket
-def accept_cli(sv_socket): 
+
+
+def accept_cli(sv_socket):
     while True:
-        cli_socket, cli_add= sv_socket.accept()
-        print 
+        cli_socket, cli_add = sv_socket.accept()
+        print
         client_list.append(cli_socket)
         # print(cli_add[1])
-        list_name.append(str(cli_add[1]%100))
+        list_name.append(str(cli_add[1] % 100))
         print('Accepted socket')
-        send_th=threading.Thread(target=send_cli,args=[cli_socket])  
-      
+        send_th = threading.Thread(target=send_cli, args=[cli_socket])
+
         send_th.start()
 
-#send to all client except one(who send)        
+#send to all client except one(who send)
+
+
 def send_cli(cli_socket):
     while True:
        # print(cli_socket)
         msg = cli_socket.recv(M_SIZE)
-        if (len(msg)==0):
-            msg={'type':'O','msg':''}
+        if (len(msg) == 0):
+            msg = {'type': 'O', 'msg': ''}
         else:
             msg = json.loads(msg.decode())
-        if msg['type']=='J':
-            list_name[client_list.index(cli_socket)]= msg["msg"] +"-"+ list_name[client_list.index(cli_socket)]
-            print(f'Client {list_name[client_list.index(cli_socket)]} send msg: JOIN')
+        if msg['type'] == 'J':
+            list_name[client_list.index(
+                cli_socket)] = msg["msg"] + "-" + list_name[client_list.index(cli_socket)]
+            print(
+                f'Client {list_name[client_list.index(cli_socket)]} send msg: JOIN')
 
         for client in client_list:
             if client != cli_socket:
-                if msg['type']=='J':                    
+                if msg['type'] == 'J':
                     client.send(f'\t<<<{msg["msg"]}>>> JOIN'.encode())
-                elif msg['type']=='N':
-                    if msg["msg"]=='':
+                elif msg['type'] == 'N':
+                    if msg["msg"] == '':
                         continue
-                    client.send(f'<{datetime.now().strftime("%H:%M")}>[{list_name[client_list.index(cli_socket)]}]:{msg["msg"]}'.encode())
+                    client.send(
+                        f'<{datetime.now().strftime("%H:%M")}>[{list_name[client_list.index(cli_socket)]}]:{msg["msg"]}'.encode())
                     # print(f'Client {list_name[client_list.index(cli_socket)]} send msg: {msg["msg"]}')
-                elif msg['type']=='O':
-                    client.send(f'\t <<<{list_name[client_list.index(cli_socket)]}>>> OUT'.encode())
+                elif msg['type'] == 'O':
+                    client.send(
+                        f'\t <<<{list_name[client_list.index(cli_socket)]}>>> OUT'.encode())
 
-        if msg['type']=='O':                            
+        if msg['type'] == 'O':
             print(f'Client {list_name[client_list.index(cli_socket)]} OUT')
             list_name.pop(client_list.index(cli_socket))
             client_list.remove(cli_socket)
-            return 1            
+            return 1
 #server
+
+
 def sv():
-    sv_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sv_socket.bind((HOST,PORT))
+    sv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sv_socket.bind((HOST, PORT))
     sv_socket.listen(5)
-    accept_th =threading.Thread(target=accept_cli, args=[sv_socket])
+    accept_th = threading.Thread(target=accept_cli, args=[sv_socket])
     accept_th.start()
 
     #keyboard Interrupt
@@ -70,4 +80,6 @@ def sv():
             cli.send(f'-1'.encode())
             cli.close()
         os._exit(0)
+
+
 sv()
