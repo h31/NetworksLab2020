e0e073fee6ed9fe8105f218c184f1ca2d9b4e0d4
tdw <tdw31@tdw.localdomain>
2020-10-22 00:45:45 +0300

    first commit for lab2

--- py/server.py
+++ py/server.py
@@ -1,70 +1,100 @@
 import socket
 import threading
-import json
 from datetime import datetime
 import os
+import random
+import select
+HOST = "127.0.0.1"
+PORT = 5001
+SIZE = 4
+sockets_list=[]
+client_list = []
+list_name = []
 
-HOST ='127.0.0.1'
-PORT =5002
-M_SIZE=1024
-client_list=[]
-list_name=[]
-#accept client_socket
-def accept_cli(sv_socket): 
-    while True:
-        cli_socket, cli_add= sv_socket.accept()
-        print 
-        client_list.append(cli_socket)
-        list_name.append(str(cli_add[1]%100))
-        print('Accepted socket')
-        send_th=threading.Thread(target=send_cli,args=[cli_socket])  
-      
-        send_th.start()
 
-#send to all client except one(who send)        
-def send_cli(cli_socket):
-    while True:
-        msg = cli_socket.recv(M_SIZE)
-        if (len(msg)==0):
-            msg={'type':'O','msg':''}
-        else:
-            msg = json.loads(msg.decode())
-        if msg['type']=='J':
-            list_name[client_list.index(cli_socket)]= msg["msg"] +"-"+ list_name[client_list.index(cli_socket)]
-            print(f'Client {list_name[client_list.index(cli_socket)]} send msg: JOIN')
-
-        for client in client_list:
-            if client != cli_socket:
-                if msg['type']=='J':                    
-                    client.send(f'\t<<<{msg["msg"]}>>> JOIN'.encode())
-                elif msg['type']=='N':
-                    if msg["msg"]=='':
-                        continue
-                    client.send(f'<{datetime.now().strftime("%H:%M")}>[{list_name[client_list.index(cli_socket)]}]:{msg["msg"]}'.encode())
-                elif msg['type']=='O':
-                    client.send(f'\t <<<{list_name[client_list.index(cli_socket)]}>>> OUT'.encode())
-
-        if msg['type']=='O':                            
-            print(f'Client {list_name[client_list.index(cli_socket)]} OUT')
-            list_name.pop(client_list.index(cli_socket))
-            client_list.remove(cli_socket)
-            return 1            
-#server
 def sv():
-    sv_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sv_socket.bind((HOST,PORT))
+    sv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # reusable right after sudden shutdown
+    sv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sv_socket.bind((HOST, PORT))
     sv_socket.listen(5)
-    accept_th =threading.Thread(target=accept_cli, args=[sv_socket])
-    accept_th.start()
-
-    #keyboard Interrupt
+    sockets_list.append(sv_socket)
+    # keyboard Interrupt
     try:
         while 1:
-            continue
+            read_s, write_s, error_s= select.select(sockets_list,[],[])
+            for s in read_s:
+                if s==sv_socket:
+                    accept_cli(sv_socket)
+                else:
+                    recv_send_cli(s)
+                    
     except KeyboardInterrupt:
-        sv_socket.close()
         for cli in client_list:
-            cli.send(f'-1'.encode())
-            cli.close()
+            cli.send(f"-1".encode())
+        sv_socket.close()
         os._exit(0)
+
+
+
+# accept client_socket
+def accept_cli(sv_socket):
+    cli_socket, cli_add = sv_socket.accept()
+    sockets_list.append(cli_socket)
+    client_list.append(cli_socket)
+    list_name.append("")
+    print("Accepted socket")
+
+# recv and send to all client except one(who send)
+def recv_send_cli(cli_socket):
+    length = int(cli_socket.recv(SIZE))
+    type_m = int(cli_socket.recv(SIZE).decode("UTF-8"))
+    msg = cli_socket.recv(length).decode("UTF-8")
+    # set name
+    name = get_set_name(cli_socket, msg, type_m)
+    # set time
+    time = datetime.now().strftime("%H:%M")
+    # change message format
+    msg_cli = format_msg(cli_socket, type_m, msg, time, name)
+    send_to_client(cli_socket, type_m, name, msg_cli)
+    if type_m == -1:
+        print('')
+        list_name.pop(client_list.index(cli_socket))
+        client_list.remove(cli_socket)
+        sockets_list.remove(cli_socket)   
+
+def send_to_client(cli_socket, type_m, name, msg):
+    for client in client_list:
+        if client != cli_socket:
+            length = f"{len(msg):<{SIZE}}".encode("UTF-8")
+            client.send(length + msg)
+def get_set_name(cli_socket, msg, type_m):
+    if type_m == 2:
+        indx = client_list.index(cli_socket)
+        while 1:
+            temp = msg + "-" + str(random.randint(0,100))
+            if temp not in list_name:
+                break
+        list_name[indx] = temp
+    return list_name[client_list.index(cli_socket)]
+
+
+def format_msg(cli_socket, type_m, msg, time, name):
+    new_format = ""
+    #'2'<-> join message
+    if type_m == 2:
+        new_format = f"\t<<<{name} JOIN>>>"
+    #'1'<-> normal message
+    elif type_m == 1:
+        new_format = f"<{time}>[{name}]:{msg}"
+    #'-1'<-> out message
+    elif type_m == -1:
+        print('ok')
+        new_format = f"\t<<<{name}>>> OUT"
+    return new_format.encode("UTF-8")
+
+
+# server
+
+
 sv()
