822955bbd013d82fb825c59737ae0d0e62755093
nhanthuhai <nhanthuhai@pop-os.localdomain>
2020-10-29 23:16:42 +0300

    Modify client

--- tcp_lab/server.py
+++ tcp_lab/server.py
@@ -2,10 +2,7 @@
 import socket
 from datetime import datetime
 import os
-<<<<<<< HEAD
-=======
 import select
->>>>>>> 16365c9a38309f99b117de6282e80a93a9d60654
 
 HEADER_LENGTH = 10
 
@@ -24,18 +21,6 @@ def main():
 
     try:
         while True:
-<<<<<<< HEAD
-            cli_sock, cli_addr = server.accept()
-            nickname = receive_msg(cli_sock)
-
-            if nickname:
-                clients[cli_sock] = nickname
-                current_time = datetime.now().strftime("%H:%M")
-                print(f"At {current_time} New client has connected")
-                notify('+1', cli_sock)
-                handler_thread = threading.Thread(target=handler_client, args=(cli_sock, ))
-                handler_thread.start()
-=======
             readable_sock, writable_sock, exception_sock = select.select(
                 sockets, [], [])
             for sock_fd in readable_sock:
@@ -55,7 +40,6 @@ def main():
                     if hander is False:
                         continue
 
->>>>>>> 16365c9a38309f99b117de6282e80a93a9d60654
     except KeyboardInterrupt:
         for cl in clients:
             cl.shutdown(socket.SHUT_WR)
@@ -69,10 +53,7 @@ def broadcast(msg, cli_sock):
         if client != cli_sock:
             client.send(msg)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 16365c9a38309f99b117de6282e80a93a9d60654
 def notify(typeOfn, cli_sock):
     code_n = f'{typeOfn:<{HEADER_LENGTH}}'.encode('utf-8')
     nickname = clients[cli_sock]
@@ -97,45 +78,16 @@ def receive_msg(cli_sock):
 
         msg_length = int(msg_header.decode('utf-8').strip())
 
-<<<<<<< HEAD
-        return {'header': msg_header, 'data': cli_sock.recv(msg_length)}
-    
-=======
         msg = cli_sock.recv(msg_length)
 
         return {'header': msg_header, 'data': msg}
 
->>>>>>> 16365c9a38309f99b117de6282e80a93a9d60654
     except ValueError:
         print("Type of header must be 'int'")
         return False
     
     except:
         return False
-<<<<<<< HEAD
-      
-def handler_client(cli_sock):
-    while True:
-        msg = receive_msg(cli_sock)
-        nickname = clients[cli_sock]
-        if msg is False:
-            cli_sock.shutdown(socket.SHUT_WR)
-            cli_sock.close()         
-            note = f"{nickname['data'].decode('utf-8')} has disconnected"
-            print(note)
-            notify('-1', cli_sock)
-            del clients[cli_sock]
-            return None
-        
-        send_time = receive_msg(cli_sock)
-        
-        current_time = datetime.now().strftime("%H:%M")
-
-        print(f'At {current_time} received message from {nickname["data"].decode("utf-8")}: {msg["data"].decode("utf-8")}')
-
-        full_msg = nickname['header'] + nickname['data'] + msg['header'] + msg['data'] + send_time['header'] + send_time['data']
-        broadcast(full_msg, cli_sock)
-=======
 
 
 def handler_client(cli_sock):
@@ -160,7 +112,6 @@ def handler_client(cli_sock):
 
     full_msg = nickname['header'] + nickname['data'] + msg['header'] + msg['data'] + send_time['header'] + send_time['data']
     broadcast(full_msg, cli_sock)
->>>>>>> 16365c9a38309f99b117de6282e80a93a9d60654
 
 
 if __name__ == '__main__':
