88ad0962c1d760e2445a6560619b54c8493a198c
D4ker <Alex-Daker@yandex.ru>
2020-10-31 17:28:54 +0300

    First stable version

--- lab1/bat/close.bat
+++ lab1/bat/close.bat
@@ -1,4 +1,2 @@
 @echo off
 taskkill /fi "imagename eq cmd.exe" /fi "Windowtitle ne cmd.exe - taskkill*"
-:: taskkill /im cmd.exe
-:: exit
--- lab1/bat/test.bat
+++ lab1/bat/test.bat
@@ -1,6 +1,4 @@
 @echo off
-:: cd /d D:\Projects\py\NetworksLab2020\lab1\bat
 start close.bat /wait
-start cmd.exe /c server_start.bat
-for /l %%i in (1,1,3) do start cmd.exe /c client_start.bat %%i
-:: exit
+start cmd.exe /k server_start.bat
+for /l %%i in (1,1,3) do start cmd.exe /k client_start.bat %%i
