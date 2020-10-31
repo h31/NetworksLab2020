ebef67e7baa547d4e686ccf76364716ad6c4d51a
D4ker <Alex-Daker@yandex.ru>
2020-10-31 14:55:01 +0300

    Modify client

--- lab1/client.py
+++ lab1/client.py
@@ -1,69 +1,34 @@
-import socket, time, threading
+import socket
+import threading
 
-key = 8194
+nickname = input("Nickname: ")
 
-shutdown = False
-join = False
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 44444))
 
-# Позволяет принимать данные другого пользователя
-def receving(name, sock):
-    while not shutdown:
+def receive():
+    while True:
         try:
-            while True:
-                data, addr = sock.recvfrom(1024)
-                #print(data.decode("utf-8"))
-
-                dataSplit = data.decode("utf-8").split(': ', maxsplit=1)
-                decrypt = dataSplit[0]
-                if len(dataSplit) == 2:
-                    decrypt += ": "
-                    for i in dataSplit[1]:
-                        decrypt += chr(ord(i) ^ key)
-                print(decrypt)
-
-                time.sleep(0.2)
+            message = client.recv(1024).decode("ascii")
+            if message == "NICK":
+                client.send(nickname.encode("ascii"))
+            else:
+                print(message)
         except:
-            pass
+            print("Error")
+            client.close()
+            break
 
-host = socket.gethostbyname(socket.gethostname())  # принимает в себе ip
-port = 0  # порт. 0 так как только подключаемся
+def write():
+    while True:
+        message = f'[{nickname}]: {input("")}'
+        client.send(message.encode("ascii"))
 
-server = ("192.168.56.1", 9090)
+receiveThread = threading.Thread(target = receive)
+receiveThread.start()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # объявляем переменную s содержащую сокеты (2 - udp, 1 - ip)
-s.bind((host, port))  # объявляем технологию на создание
-s.setblocking(0)
-
-alias = input("Name: ")
-
-rT = threading.Thread(target = receving, args = ("RecvThread", s))
-rT.start()
-
-while shutdown == False:
-    if join == False:
-        # Если только подключились, отправить сообщение об этом на сервер
-        s.sendto(("[" + alias + "] => join chat").encode("utf-8"), server)
-        join = True
-    else:
-        try:
-            message = input()
-
-            crypt = ""
-            for i in message:
-                crypt += chr(ord(i) ^ key)
-            message = crypt
-
-            # Если отправили не пустоту
-            if message != "":
-                s.sendto(("[" + alias + "]: " + message).encode("utf-8"), server)
-
-            time.sleep(0.2)
-        except:
-            s.sendto(("[" + alias + "] <= left chat").encode("utf-8"), server)
-            shutdown = True
-
-rT.join()
-s.close()
+writeThread = threading.Thread(target = write)
+writeThread.start()
 
 
 
