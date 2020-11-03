0da325b3f513e8cb6629c1f4f4f74fbce0bcd9e1
D4ker <Alex-Daker@yandex.ru>
2020-11-03 18:54:33 +0300

    Fixed bugs and improved the code

--- lab1/client.py
+++ lab1/client.py
@@ -3,56 +3,68 @@ import threading
 import time
 from datetime import datetime
 
-BUFSIZE = 256
+HEADER = 10
 TIMEZONE = time.timezone
-
-serverIP = "127.0.0.1"
+CLIENT_WORKING = True
+SERVER_IP = "127.0.0.1"
 # serverIP = "51.15.130.137"
-port = 44444
+SERVER_PORT = 44450
 
 clientNickname = input("Nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((serverIP, port))
+try:
+    client.connect((SERVER_IP, SERVER_PORT))
+except ConnectionRefusedError:
+    print(f"Server ({SERVER_IP}:{SERVER_PORT}) is not available")
+    exit(0)
 
 def clientTime():
     return datetime.timestamp(datetime.now()) + TIMEZONE
 
 def getData(time, nickname, message):
     data = f"{time}\0{nickname}\0{message}".encode("utf-8")
-    return f"{len(data):<{BUFSIZE}}".encode("utf-8") + data
+    return f"{len(data):<{HEADER}}".encode("utf-8") + data
 
 # Функция для обработки информации, приходящей с сервера
 def receive():
-    while True:
+    global CLIENT_WORKING
+    data = getData(clientTime(), clientNickname, "POST_NICKNAME")
+    client.send(data)
+    while CLIENT_WORKING:
         try:
-            messageLength = int(client.recv(BUFSIZE).decode("utf-8"))
-            rawData = client.recv(BUFSIZE)
+            messageLength = int(client.recv(HEADER).decode("utf-8"))
+            rawData = client.recv(messageLength)
             currentLength = len(rawData)
             while messageLength != currentLength:
-                rawData += client.recv(BUFSIZE)
+                rawData += client.recv(messageLength)
                 currentLength = len(rawData)
             data = rawData.decode("utf-8")
             serverTime, nickname, message = data.split("\0")
-            if nickname == "Server" and message == "\1GET_NICKNAME":
-                data = getData(clientTime(), clientNickname, "\1POST_NICKNAME")
-                client.send(data)
-            else:
-                # Прибавляем к времени наш часовой пояс
 
-                messageTime = datetime.strftime(datetime.fromtimestamp(float(serverTime) - TIMEZONE), "%Y-%m-%d-%H.%M.%S")
-                print(f"<{messageTime}> [{nickname}] {message}")
-        except:
+            # Прибавляем к времени наш часовой пояс
+            messageTime = datetime.strftime(datetime.utcfromtimestamp(float(serverTime) - TIMEZONE), "%Y-%m-%d-%H.%M.%S")
+            print(f"<{messageTime}> [{nickname}] {message}")
+        except ConnectionResetError or KeyboardInterrupt:
             print("Error")
+            client.close()
+            break
+        except ValueError:
+            CLIENT_WORKING = False
+            print("Server close connection")
             client.close()
             break
 
 # Функция для отправки сообщений на сервер
 def write():
-    while True:
+    global CLIENT_WORKING
+    while CLIENT_WORKING:
         message = input("")
         data = getData(clientTime(), clientNickname, message)
-        client.send(data)
+        if CLIENT_WORKING:
+            client.send(data)
+        else:
+            break
 
 receiveThread = threading.Thread(target = receive)
 receiveThread.start()
--- lab1/server.py
+++ lab1/server.py
@@ -3,32 +3,31 @@ import threading
 import time
 from datetime import datetime
 
-BUFSIZE = 256
+HEADER = 10
 TIMEZONE = time.timezone
-
-host = "127.0.0.1"  # ip сервера (localhost)
-# host = "0.0.0.0"  # ip сервера (localhost)
-port = 44444  # порт
+SERVER_WORKING = True
+HOST = "127.0.0.1"  # ip сервера (localhost)
+# HOST = "0.0.0.0"
+PORT = 44450  # порт
+SERVER_NICKNAME = "Server"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind((HOST, PORT))
 
 server.listen()
-
-serverNickname = "Server"
 
 clients = []  # адреса (!) подключенных клиентов
 nicknames = []
 
 def getData(time, nickname, message):
     data = f"{time}\0{nickname}\0{message}".encode("utf-8")
-    return f"{len(data):<{BUFSIZE}}".encode("utf-8") + data
+    return f"{len(data):<{HEADER}}".encode("utf-8") + data
 
 def serverTime():
     return datetime.timestamp(datetime.now()) + TIMEZONE
 
 def serverTimeFormat(mytime):
-    return datetime.strftime(datetime.fromtimestamp(mytime), "%Y-%m-%d-%H.%M.%S")
+    return datetime.strftime(datetime.utcfromtimestamp(mytime), "%Y-%m-%d-%H.%M.%S")
 
 def printLog(time, message):
     print(f"[{time}]/[log]: {message}")
@@ -41,13 +40,13 @@ def broadcast(muteClient, data):
 
 # Функция для работы с клиентом. Получаем сообщения и обрабатываем их
 def handle(client, addres):
-    while True:
+    while SERVER_WORKING:
         try:
-            messageLength = int(client.recv(BUFSIZE).decode("utf-8"))
-            rawData = client.recv(BUFSIZE)
+            messageLength = int(client.recv(HEADER).decode("utf-8"))
+            rawData = client.recv(messageLength)
             currentLength = len(rawData)
             while messageLength != currentLength:
-                rawData += client.recv(BUFSIZE)
+                rawData += client.recv(messageLength)
                 currentLength = len(rawData)
             data = rawData.decode("utf-8")
             receiptTime = serverTime()
@@ -55,7 +54,7 @@ def handle(client, addres):
             newData = getData(receiptTime, nickname, message)
             printLog(serverTimeFormat(receiptTime), f"[{addres[0]}:{str(addres[1])}]/[{nickname}] -> {message}")
             broadcast(client, newData)
-        except:
+        except ConnectionResetError:
             currentTime = serverTime()
             clientIndex = clients.index(client)
             clients.remove(client)
@@ -64,31 +63,42 @@ def handle(client, addres):
             nicknames.remove(nickname)
             leftmsg = f"[{nickname}] left the chat"
             printLog(serverTimeFormat(currentTime), leftmsg)
-            data = getData(currentTime, serverNickname, leftmsg)
+            data = getData(currentTime, SERVER_NICKNAME, leftmsg)
             broadcast(client, data)
             break
 
 # Функция для обработки подключения пользователей к серверу
 def receive():
-    while True:
-        client, addres = server.accept()
-        printLog(serverTimeFormat(serverTime()), f"Connected with {str(addres)}")
+    global SERVER_WORKING
+    try:
+        while SERVER_WORKING:
+            client, addres = server.accept()
+            printLog(serverTimeFormat(serverTime()), f"Connected with {str(addres)}")
 
-        data = getData(serverTime(), serverNickname, "\1GET_NICKNAME")
-        client.send(data)
-        nickname = client.recv(1024).decode("utf-8").split("\0")[1]
-        nicknames.append(nickname)
-        clients.append(client)
+            # Получаем ник пользователя
+            messageLength = int(client.recv(HEADER).decode("utf-8"))
+            nickname = client.recv(messageLength).decode("utf-8").split("\0")[1]
+            nicknames.append(nickname)
+            clients.append(client)
 
-        currentTime = serverTime()
-        joinmsg = f"[{nickname}] joined the chat"
-        printLog(serverTimeFormat(currentTime), joinmsg)
-        data = getData(currentTime, serverNickname, joinmsg)
-        client.send(data)
-        broadcast(client, data)
+            currentTime = serverTime()
+            joinmsg = f"[{nickname}] joined the chat"
+            printLog(serverTimeFormat(currentTime), joinmsg)
+            data = getData(currentTime, SERVER_NICKNAME, joinmsg)
+            client.send(data)
+            broadcast(client, data)
 
-        thread = threading.Thread(target = handle, args = (client, addres))
-        thread.start()
+            thread = threading.Thread(target = handle, args = (client, addres))
+            thread.start()
+    except KeyboardInterrupt:
+        SERVER_WORKING = False
+        print("---Server Stopped---")
+        newData = getData(serverTime(), SERVER_NICKNAME, "Server shutdown")
+        broadcast(None, newData)
+        for client in clients:
+            client.close()
+        server.close()
+        exit(0)
 
 print("---Server Started---")
 receive()
