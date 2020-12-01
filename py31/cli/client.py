b7a679f3524bb90ba8690b963244c9c6bbf976e6
tdw <tdw31@tdw.localdomain>
2020-12-01 12:11:28 +0300

    now work with others tftp client and server

--- py31/cli/client.py
+++ py31/cli/client.py
@@ -1,8 +1,10 @@
 import socket
 import os
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = "192.168.0.109"
+PORT = 69
+sv_addr = (HOST, PORT)
+
 DATA_LENGTH = 512
 
 opcode_rq = {
@@ -24,9 +26,10 @@ error_msg = {
     7: "No such user.",
 }
 
+cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 def main():
-    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while 1:
         inp = input().split(" ")
         if len(inp) != 2:
@@ -35,22 +38,24 @@ def main():
             type_RQ = inp[0]
             fileName = inp[1]
             if type_RQ == "r":
-                recv_File(cli, opcode_rq["RRQ"], fileName)
+                recv_File(opcode_rq["RRQ"], fileName)
             elif type_RQ == "w":
-                send_File(cli, opcode_rq["WRQ"], fileName)
+                send_File(opcode_rq["WRQ"], fileName)
             else:
                 print("Syntax problem")
 
 
 # create and send READ WRITE request
-def create_RQ(cli, mode, fileName):
+def create_RQ(mode, fileName):
     rq = bytearray()
     rq += mode.to_bytes(2, "big")
     rq += fileName.encode("utf_8")
     rq.append(0)
     rq += "octet".encode("utf_8")
     rq.append(0)
-    cli.sendto(rq, (HOST, PORT))
+    cli.sendto(rq, sv_addr)
+    print(len(rq))
+    print(rq)
 
 
 # Packing data
@@ -63,26 +68,26 @@ def packing_data(block, data):
 
 
 # get ack, throw error if have
-def recv_Ack(cli):
-    rq, _ = cli.recvfrom(516)
+def recv_Ack():
+    rq, server = cli.recvfrom(516)
     opcode = int.from_bytes(rq[0:2], "big")
     if opcode != opcode_rq["ACK"]:
         if opcode != opcode_rq["ERR"]:
-            return opcode_rq["ERR"], 4
+            return opcode_rq["ERR"], 4, server
         else:
-            return opcode_rq["ERR"], int.from_bytes(rq[2:4], "big")
+            return opcode_rq["ERR"], int.from_bytes(rq[2:4], "big"), server
     block = int.from_bytes(rq[2:], "big")
-    return opcode, block
+    return opcode, block, server
 
 
 # send file to server
-def send_File(cli, mode, fileName):
+def send_File(mode, fileName):
     try:
         cli.settimeout(2)
         fileOpen = open(fileName, "rb")
-        create_RQ(cli, mode, fileName)
+        create_RQ(mode, fileName)
         # check ack0. throw error if have
-        op, ack0 = recv_Ack(cli)
+        op, ack0, server = recv_Ack()
         if op == opcode_rq["ERR"]:
             print(error_msg[ack0])
             return 0
@@ -90,18 +95,22 @@ def send_File(cli, mode, fileName):
         while 1:
             data = fileOpen.read(512)
             pkg = packing_data(block, data)
-            cli.sendto(pkg, (HOST, PORT))
+            cli.sendto(pkg, server)
+            print(server)
             # print error if have and send to client
-            opcode, block_ACK = recv_Ack(cli)
+            opcode, block_ACK, server = recv_Ack()
+            print(opcode)
             if opcode == opcode_rq["ERR"]:
                 error_code = block_ACK
                 print(error_msg[error_code])
                 return 0
+            print(block_ACK)
             # illegal error
             if block_ACK != block:
-                cli.sendto(error_RQ(4), (HOST, PORT))
+                cli.sendto(error_RQ(4), server)
                 return 0
             if len(data) < DATA_LENGTH:
+                print(block_ACK)
                 break
             block += 1
     except FileNotFoundError:
@@ -113,20 +122,24 @@ def send_File(cli, mode, fileName):
 
 
 # send ack to client
-def send_ACK(cli, data, sv):
+def send_ACK(data, sv):
     ack = bytearray(data)
+    ack[0] = 0
     ack[1] = opcode_rq["ACK"]
     cli.sendto(ack, sv)
 
 
-# receive file from sever
-def recv_File(cli, mode, fileName):
-    create_RQ(cli, mode, fileName)
+# receive file from server
+def recv_File(mode, fileName):
+    create_RQ(mode, fileName)
     fileOpen = open(fileName, "wb")
     cli.settimeout(2)
     while 1:
         try:
-            rq, _ = cli.recvfrom(516)
+            rq, server = cli.recvfrom(516)
+            print(server)
+
+            print(len(rq))
             # handle error if have
             if int.from_bytes(rq[0:2], "big") == opcode_rq["ERR"]:
                 error_code = int.from_bytes(rq[2:4], "big")
@@ -134,11 +147,12 @@ def recv_File(cli, mode, fileName):
                 print(error_msg[error_code])
                 os.remove(fileName)
                 break
-            send_ACK(cli, rq[0:4], (HOST, PORT))
+            send_ACK(rq[0:4], server)
             data = rq[4:]
             fileOpen.write(data)
             # end of data
             if len(data) < DATA_LENGTH:
+                print(int.from_bytes(rq[2:4], "big"))
                 break
         except socket.timeout:
             print("Time OUT")
--- py31/sv/server.py
+++ py31/sv/server.py
@@ -1,8 +1,9 @@
 import socket
 import os
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = "0.0.0.0"
+PORT69 = 69
+PORT = 50000
 DATA_LENGTH = 512
 
 opcode_rq = {
@@ -12,7 +13,6 @@ opcode_rq = {
     "ACK": 4,
     "ERR": 5,
 }
-
 error_msg = {
     0: "Not defined",
     1: "File not found.",
@@ -23,27 +23,30 @@ error_msg = {
     6: "File already exists.",
     7: "No such user.",
 }
+sv69 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sv69.bind((HOST, PORT69))
+
+sv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sv.bind((HOST, PORT))
 
 
 def main():
-    sv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sv.bind((HOST, PORT))
+
     while 1:
-        rq, cli = sv.recvfrom(516)
-        print(rq)
-        RQ_Packet(sv, rq, cli)
+        rq, cli = sv69.recvfrom(516)
+        RQ_Packet(rq, cli)
 
 
 # HANDLE READ AND WRITE REQUEST
-def RQ_Packet(sv, rq, cli):
+def RQ_Packet(rq, cli):
     opcode = int.from_bytes(rq[0:2], "big")
     param = rq[2:].split(b"\x00")
     fileName = param[0].decode("utf_8")
     mode = param[1].decode("utf_8")
     if opcode == opcode_rq["RRQ"]:
-        send_File(sv, fileName, cli)
+        send_File(fileName, cli)
     elif opcode == opcode_rq["WRQ"]:
-        recv_File(sv, fileName, cli)
+        recv_File(fileName, cli)
     else:
         sv.sendto(error_RQ(4), cli)
 
@@ -58,20 +61,20 @@ def packing_data(block, data):
 
 
 # get ack, throw error if have
-def recv_Ack(sv):
-    rq, _ = sv.recvfrom(516)
+def recv_Ack():
+    rq, client = sv.recvfrom(516)
     opcode = int.from_bytes(rq[0:2], "big")
     if opcode != opcode_rq["ACK"]:
         if opcode != opcode_rq["ERR"]:
-            return opcode_rq["ERR"], 4
+            return opcode_rq["ERR"], 4, client
         else:
-            return opcode_rq["ERR"], int.from_bytes(rq[2:4], "big")
+            return opcode_rq["ERR"], int.from_bytes(rq[2:4], "big"), client
     block = int.from_bytes(rq[2:], "big")
-    return opcode, block
+    return opcode, block, client
 
 
 # Send file, if file doesnt exist on server, send error
-def send_File(sv, fileName, cli):
+def send_File(fileName, cli):
     block = 1
     try:
         fileOpen = open(fileName, "rb")
@@ -80,17 +83,19 @@ def send_File(sv, fileName, cli):
             pkg = packing_data(block, data)
             sv.sendto(pkg, cli)
             # print error if have and send to client
-            opcode, block_ACK = recv_Ack(sv)
+            opcode, block_ACK, client = recv_Ack()
             if opcode == opcode_rq["ERR"]:
                 error_code = block_ACK
-                sv.sendto(error_RQ(4), cli)
+                sv.sendto(error_RQ(4), client)
                 print(error_msg[error_code])
                 return 0
             # illegal error
             if block_ACK != block:
-                sv.sendto(error_RQ(4), cli)
+                sv.sendto(error_RQ(4), client)
                 return 0
             if len(data) < DATA_LENGTH:
+                print(block_ACK)
+
                 break
             block += 1
     except FileNotFoundError:
@@ -100,14 +105,14 @@ def send_File(sv, fileName, cli):
 
 
 # send ack to client
-def send_ACK(sv, data, cli):
+def send_ACK(data, cli):
     ack = bytearray(data)
     ack[1] = opcode_rq["ACK"]
     sv.sendto(ack, cli)
 
 
 # receive file from client if file exist throw error
-def recv_File(sv, fileName, cli):
+def recv_File(fileName, cli):
     if os.path.exists(fileName):
         sv.sendto(error_RQ(6), cli)
         return 0
@@ -117,11 +122,11 @@ def recv_File(sv, fileName, cli):
     ack0.append(opcode_rq["ACK"])
     ack0.append(0)
     ack0.append(0)
-    send_ACK(sv, ack0, cli)
+    send_ACK(ack0, cli)
 
     fileOpen = open(fileName, "wb")
     while 1:
-        rq, _ = sv.recvfrom(516)
+        rq, client = sv.recvfrom(516)
         # handle error if have
         if int.from_bytes(rq[0:2], "big") == opcode_rq["ERR"]:
             error_code = int.from_bytes(rq[2:4], "big")
@@ -129,7 +134,7 @@ def recv_File(sv, fileName, cli):
             os.remove(fileName)
             break
         # send ack
-        send_ACK(sv, rq[0:4], cli)
+        send_ACK(rq[0:4], client)
         data = rq[4:]
         fileOpen.write(data)
         # end of data
